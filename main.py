f42e20023cc668451d10e999380c010d485cb2c8
VulcanoGal <57453205+VulcanoGal@users.noreply.github.com>
2021-10-03 21:43:50 +0200

    Avances
    Creación de parte del esqueleto del menú

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import mysql.connector
 print('¡Hola, Bienvenido a Password Manager!')
+while True:
+	try:
+		print("""
+					---- Menú ----
+			1º Introducir unas credenciales nuevas
+			2º Consultar credenciales por sitio
+			3º Consultar credenciales por correo electrónico
+			4º Consultar credenciales por usuario
+			5º Eliminar credenciales
+			6º Salir
+			""")
+	except:
+		print("Ha ocurrido un error")
