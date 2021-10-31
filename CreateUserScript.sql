5b06b4d73f48043afb90ae290b0b78cce6798132
VulcanoGal <57453205+VulcanoGal@users.noreply.github.com>
2021-10-31 16:26:16 +0100

    Mejoras en el script

---
+++ CreateUserScript.sql
@@ -0,0 +1,2 @@
+    CREATE USER 'PasswordManager'@'localhost' IDENTIFIED BY 'abc123.';
+    GRANT ALL PRIVILEGES ON * . * TO 'PasswordManager'@'localhost';
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Importamos o conector MySQL para que este script poida comunicarse co server MySQL que instalamos previamente
 import mysql.connector
+import sys
 print('¡Hola, Bienvenido a Password Manager!')
 MasterUser = ''
 MasterContra = ''
@@ -22,43 +23,40 @@ PMDB = mysql.connector.connect(host = 'localhost', database= 'PasswordManagerDB'
 PMcursor = PMDB.cursor()
 PMcursor.execute("SELECT COUNT(*) FROM information_schema.tables WHERE table_name = '"+ MasterTable +"' ;")
 checkTB = PMcursor.fetchone()
-if checkTB[0] == 0:
-	#Damoslle ao usuario a opción de crear unha nova tabla en caso de que el queira, e a opción de non, en caso de que se equivocase escribindo
-	print ("Esa tabla non existe, quere creala?")
-	PMcursor.execute("""CREATE TABLE """+ MasterTable +""" (Tipo INT NOT NULL,PRIMARY KEY (Tipo)) ;""")
-elif checkTB[0] == 1:
+
 	#Bucle como menú no cal manexamos a BBDD
-	while True:
-		try:
-			print("""
-					---- Menú ----
-				1º Introducir unas credenciales nuevas
-				2º Consultar credenciales por sitio
-				3º Consultar credenciales por correo electrónico
-				4º Consultar credenciales por usuario
-				5º Eliminar credenciales
-				6º Salir
-				""")
-			opc = int(input("Introduzca uno de los siguientes números para escoger una opción \n"))
-			if opc == 1:
-				print("opc1")
-			elif opc == 2:
-				print("opc2")
-			elif opc == 3:
-				print("opc3")
-			elif opc == 4:
-				print("opc4")
-			elif opc == 5:
-				print("opc5")
-			elif opc == 6:
-				print("Salir")
-				break
-			else:
-			 print("No ha introducido un número que se corresponda con las opciones, vuelva a intentarlo de nuevo")
-		except:
-			print("Ha ocurrido un error")
+while True:
+	if checkTB[0] == 0:
+		#Damoslle ao usuario a opción de crear unha nova tabla en caso de que el queira, e a opción de non, en caso de que se equivocase escribindo
+		opc = int( input ("Esa tabla non existe, quere creala? Pulse 1 para continuar, pulse calquera outro número para saír")[0:1])
+		if opc == 1:
+			print ("Vaise crear a nova tabla: ")
+			PMcursor.execute("""CREATE TABLE """+ MasterTable +""" (Tipo INT NOT NULL,PRIMARY KEY (Tipo)) ;""")
+		else:
+			sys.exit("Escribiu outro caracter,polo que sairá do script")
+	elif checkTB[0] == 1:
+		print("""
+				---- Menú ----
+			1º Introducir unas credenciales nuevas
+			2º Consultar credenciales por sitio
+			3º Consultar credenciales por correo electrónico
+			4º Consultar credenciales por usuario
+			5º Eliminar credenciales
+			6º Salir
+			""")
+		opc = int(input("Introduzca uno de los siguientes números para escoger una opción \n"))
+		if opc == 1:
+			print("opc1")
+		elif opc == 2:
+			print("opc2")
+		elif opc == 3:
+			print("opc3")
+		elif opc == 4:
+			print("opc4")
+		elif opc == 5:
+			print("opc5")
+		elif opc == 6:
+			print("Salir")
 			break
-else:
-	#Para evitar múltiples coincidencias e inconvenientes que poida ocasionar, pediráselle ao usuario que sexa máis específico co nome
-	# ca fin de garantizar un pouco de privacidade
-	print ("Hai demasiadas coincidencias co escrito, probe a escribir algo máis concreto por favor")
+		else:
+		 print("No ha introducido un número que se corresponda con las opciones, vuelva a intentarlo de nuevo")
