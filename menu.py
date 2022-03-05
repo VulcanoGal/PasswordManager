85372269c0bec29ebc2104d3117c634008a65463
VulcanoGal <57453205+VulcanoGal@users.noreply.github.com>
2022-03-05 16:29:42 +0100

    3

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-import mysql.connector,os,getpass,encriptacion
+import mysql.connector, os, getpass, encriptacion
 
 def login():
     global dbhost  
@@ -7,9 +7,9 @@ def login():
     global dbport
     dbhost = input("Introduzca la IP de la BBDD [localhost también es válido]: ")
     dbuser = input("Introduzca el nombre de usuario de la BBDD: ")
-    dbpassword = getpass.getpass("Introduzca la contraseña del usuario "+ dbuser +" en la BBDD: ")
+    dbpassword = getpass.getpass("Introduzca la contraseña del usuario " + dbuser + " en la BBDD: ")
     dbport = int(input("Introduzca el número del puerto de la BBDD: "))
-    initDB = mysql.connector.connect(host = dbhost, user = dbuser, password = dbpassword, port = dbport)
+    initDB = mysql.connector.connect(host=dbhost, user=dbuser, password=dbpassword, port=dbport)
     return initDB
 
 conection = login()
@@ -20,8 +20,8 @@ def createDB():
     dbname = input("Introduzca el nombre de la BBDD: ")
     initDB = conection
     cursor = initDB.cursor()
-    createDB = "CREATE DATABASE IF NOT EXISTS "+ dbname +";"
-    useDB = " USE "+ dbname +";"
+    createDB = "CREATE DATABASE IF NOT EXISTS " + dbname + ";"
+    useDB = " USE " + dbname + ";"
     createDBTable = "CREATE TABLE IF NOT EXISTS PasswordDBClient (Servicio varchar(255), Email varchar(255), Usuario varchar(255), Contraseña varchar(255), TipoServicio varchar(255))"
     createMasterTable = "CREATE TABLE IF NOT EXISTS MasterPasswd (Password varchar(255))"
     queryMasterPasswd = "SELECT * FROM MasterPasswd"
@@ -42,7 +42,6 @@ def createDB():
         print(checkMasterPasswd)
         if masterpasswd != checkMasterPasswd:
             print("La contraseña maestra es incorrecta, saliendo...")
-            exit
         else:
             print("Contraseña Maestra OK")
             
